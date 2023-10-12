8cb63e36e5aa953cd240e18b594ce1dfd11bf046
Bianca <bianca.ropiha@icloud.com>
2023-10-12 13:57:25 +1300

    fixed red hue + added drums

--- music_starter.js
+++ music_starter.js
@@ -1,35 +1,31 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+   let drumValue= 50
+   let changePosition= true
+   let drumCircleX= 0
+   let drumCircleY= 0
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
 
   colorMode(HSL, 255 , 255 ,255);
 
-   let pinkColour = color 	(315, 100, 82)
-   let purpleColour = color (277, 65, 19)
-
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
-
-
-   
   let saturation = map(song.currentTime(), 151, 206, 0, 255);
 
   fill(30, saturation, 255)
-  
+
+   let pinkColour = color 	(315, 100 * saturation, 82)
+   let purpleColour = color (277, 65* saturation, 19)
+
+  //background
   for ( let i = 0; i < 1080; i++){
    let gradientAmount = map(i, 0, 1080, 0, 1)
    let strokeColour = lerpColor(pinkColour , purpleColour , gradientAmount)
 
    stroke(strokeColour)
-   line(0, 100+i, width, 100+i)
+   line(0, 1+i, width, 1+i)
 }
 
-   // vocal bar is red
+   // vocal 
    stroke(255);
    noFill()
 
@@ -47,13 +43,25 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   
  
-    // drum bar 
+    // drum 
     fill(255)
-    if (drum > 50) {
-      ellipse(50,50,50)
+    if (drum > 75) {
+      
+      if (changePosition){
+
+            drumCircleX = random([100],[1000])
+            drumCircleY = random([100],[800])
+
+            changePosition = false;
+
+      }
+
+      ellipse(drumCircleX, drumCircleY, 50, 50)
   }
- 
-   // bass bar 
+  else{
+   changePosition= true
+  }
+   // bass  
    stroke(255);
    noFill()
 
